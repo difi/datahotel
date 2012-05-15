eff17dfaf7882c8c6ada17dbbda5bb7659772ed5
Erlend Klakegg Bergheim <erlend@klakegg.net>
2012-05-15 07:33:40 +0200

    Fixing bug in tests.

--- logic/master/src/test/java/no/difi/datahotel/logic/impl/OwnerEJBTest.java
+++ logic/master/src/test/java/no/difi/datahotel/logic/impl/OwnerEJBTest.java
@@ -55,9 +55,9 @@ public class OwnerEJBTest extends AbstractEntityManagerTest {
 		xmlOwnerField.setAccessible(true);
 		xmlOwnerField.set(xmlEJB, ownerEJB);
 
-		Field ownerXmlField = OwnerEJB.class.getDeclaredField("xmlEJB");
-		ownerXmlField.setAccessible(true);
-		ownerXmlField.set(ownerEJB, xmlEJB);
+		//Field ownerXmlField = OwnerEJB.class.getDeclaredField("xmlEJB");
+		//ownerXmlField.setAccessible(true);
+		//ownerXmlField.set(ownerEJB, xmlEJB);
 
 		DatasetEJB datasetEJB = new DatasetEJB();
 		Field datasetEmField = AbstractJPAHandler.class.getDeclaredField("em");
