85756bdb2c44d2c2d632ac248dcc5a54edd343f8
Erlend Klakegg Bergheim <erlend@klakegg.net>
2014-01-29 13:09:03 +0100

    Fix.

--- src/main/java/no/difi/datahotel/slave/logic/SearchBean.java
+++ src/main/java/no/difi/datahotel/slave/logic/SearchBean.java
@@ -4,8 +4,10 @@ import no.difi.datahotel.model.Metadata;
 import no.difi.datahotel.model.Result;
 import no.difi.datahotel.util.Filesystem;
 import org.apache.lucene.document.Fieldable;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.search.*;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.springframework.stereotype.Component;
@@ -23,7 +25,7 @@ import static no.difi.datahotel.util.Filesystem.FOLDER_CACHE_INDEX;
 public class SearchBean {
 
 	private int num = 100;
-    // private QueryParser parser = new QueryParser(IndexBean.version, "searchable", IndexBean.analyzer);
+    private QueryParser parser = new QueryParser(IndexBean.version, "searchable", IndexBean.analyzer);
 
     private Map<String, Directory> directories = new HashMap<String, Directory>();
 	private Map<String, IndexSearcher> searchers = new HashMap<String, IndexSearcher>();
@@ -49,17 +51,17 @@ public class SearchBean {
 	}
 
 	public Result find(Metadata metadata, String q, Map<String, String> lookup, int page) {
-        BooleanQuery booleanQuery = new BooleanQuery();
-        // StringBuilder query = new StringBuilder();
+        // BooleanQuery booleanQuery = new BooleanQuery();
+        StringBuilder query = new StringBuilder();
         if (lookup != null)
             for (String key : lookup.keySet()) {
-                booleanQuery.add(new BooleanClause(new WildcardQuery(new Term(key, lookup.get(key))), BooleanClause.Occur.MUST));
-                // query.append(query.length() == 0 ? "" : " AND ").append("+").append(key).append(":").append(lookup.get(key));
+                // booleanQuery.add(new BooleanClause(new WildcardQuery(new Term(key, lookup.get(key))), BooleanClause.Occur.MUST));
+                query.append(query.length() == 0 ? "" : " AND ").append("+").append(key).append(":").append(lookup.get(key));
             }
         if (q != null && !q.equals("")) {
-            booleanQuery.add(new BooleanClause(new WildcardQuery(new Term("searchable", q.toLowerCase())), BooleanClause.Occur.SHOULD));
+            // booleanQuery.add(new BooleanClause(new WildcardQuery(new Term("searchable", q.toLowerCase())), BooleanClause.Occur.SHOULD));
             // booleanQuery.add(new BooleanClause(new WildcardQuery(new Term(q.toLowerCase())), BooleanClause.Occur.SHOULD));
-            // query.append(query.length() == 0 ? "" : " AND ").append(q);
+            query.append(query.length() == 0 ? "" : " AND ").append(q);
         }
 
         Result result = new Result();
@@ -68,13 +70,13 @@ public class SearchBean {
 		IndexSearcher searcher = searchers.get(metadata.getLocation());
 		if (searcher != null) {
 			try {
-                TopDocs docs = searcher.search(booleanQuery, num * page);
+                // TopDocs docs = searcher.search(booleanQuery, num * page);
 
                 // System.out.println(query);
                 // System.out.println(parser.parse(query.toString()));
-                System.out.println(booleanQuery);
+                // System.out.println(booleanQuery);
 
-                // TopDocs docs = searcher.search(parser.parse(query.toString()), num * page);
+                TopDocs docs = searcher.search(parser.parse(query.toString()), num * page);
                 List<Map<String, String>> rdocs = convert(searcher, docs);
 
 				result.setEntries((rdocs.size() < num * (page - 1)) ? new ArrayList<Map<String, String>>() : rdocs.subList(
@@ -83,8 +85,8 @@ public class SearchBean {
                 // } catch (ParseException e) {
                 // throw new Exception("Unable to parse query.");
             } catch (Exception e) {
-                // metadata.getLogger().warning("Error in search: " + query.toString() + " - Reason: " + e.getClass().getSimpleName());
-                metadata.getLogger().warning("Error in search: " + booleanQuery.toString() + " - Reason: " + e.getClass().getSimpleName() + " - " + e.getStackTrace()[0].toString());
+                metadata.getLogger().warning("Error in search: " + query.toString() + " - Reason: " + e.getClass().getSimpleName());
+                // metadata.getLogger().warning("Error in search: " + booleanQuery.toString() + " - Reason: " + e.getClass().getSimpleName() + " - " + e.getStackTrace()[0].toString());
             }
 		}
 
